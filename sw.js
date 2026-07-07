e26be176404dfd773c3ed03c89a0684501064d55
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-07-07 21:22:55 +0900

    🔄 SW更新 vmramdeth

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mr5fv8z2';
+const CACHE = 'app-mramdeth';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
